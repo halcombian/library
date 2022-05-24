d5650f86ef2389cda9ea5fe7e0fbec5af5515d6d
Ian Halcomb <halcombian@gmail.com>
2022-05-24 01:54:29 -0400

    create add-book button and style for visibilty

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-function Book(title, author, pages, read) {
+//let myLibrary = [];
+
+/*function Book(title, author, pages, read) {
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
@@ -27,4 +29,42 @@ theVelveteenRabbit.info();
 missSuzy.info();
 madeline.info();
 whereTheWildThingsAre.info();
-theCatInTheHat.info();
+theCatInTheHat.info();*/
+
+//function addBookToLibrary() {}
+
+//------------------------------------------------------------------------
+
+//Gives functionality to "Add Book" button
+//Function creates a window in the middle of the page
+//The window contains inputs required to add a book to the page
+const addBook = document.querySelector(".add-book");
+addBook.addEventListener("click", () => {
+	const inputWindow = document.createElement("div");
+	inputWindow.className = "input-window";
+	document.body.append(inputWindow);
+
+	//Creates the close button at the top right of the window
+	const close = document.createElement("div");
+	close.setAttribute("id", "close-btn");
+	close.innerHTML = "X";
+	inputWindow.appendChild(close);
+
+	//Arrays used to give attributes to the elements created in the following loop
+	//The inputContainers are created for easier styling
+	const inputTypes = ["text", "text", "number", "radio", "radio"];
+	const inputNames = ["title", "author", "pages", "read", "unread"];
+	for (let i = 0; i < inputNames.length; i++) {
+		const inputContainer = document.createElement("div");
+		const label = document.createElement("label");
+		const input = document.createElement("input");
+		inputContainer.setAttribute("id", inputNames[i] + "-container");
+		label.setAttribute("for", inputNames[i]);
+		label.innerHTML = inputNames[i].toUpperCase() + ": ";
+		input.type = inputTypes[i];
+		input.name = inputNames[i];
+		inputWindow.appendChild(inputContainer);
+		inputContainer.appendChild(label);
+		inputContainer.appendChild(input);
+	}
+});
